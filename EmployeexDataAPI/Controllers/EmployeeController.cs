048d5fab720a8e2eb8192a7be6f79ac8730d84a8
nickattack97 <nickattack97@gmail.com>
2022-05-25 16:59:45 +0200

    controller updates

---
+++ EmployeexDataAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeexDataAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly DataContext context;
+
+        public EmployeeController(DataContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Employee>>> Get()
+        {
+            return Ok(await context.tblStaff.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> Get(Int64 id)
+        {
+            var employee = await context.tblStaff.FindAsync(id);
+            if (employee == null)
+                return BadRequest("employee not found.");
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Employee>>> Addemployee(Employee employee)
+        {
+            context.tblStaff.Add(employee);
+            await context.SaveChangesAsync();
+            return Ok(await context.tblStaff.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Employee>>> Updateemployee(Employee req)
+        {
+            var employee = await context.tblStaff.FindAsync(req.ID);
+            if (employee == null)
+                return BadRequest("employee not found.");
+            employee.Name = req.Name;
+            employee.Surname = req.Surname;
+            employee.Category = req.Category;
+            employee.Department = req.Department;
+            employee.SubUnit = req.SubUnit;
+            employee.JobTitle = req.JobTitle;
+            employee.WorkPlaceState = req.WorkPlaceState;
+            employee.Terminated = req.Terminated;
+
+            await context.SaveChangesAsync();
+
+            return Ok(await context.tblStaff.ToListAsync());
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Employee>> Delete(Int64 id)
+        {
+            var employee = await context.tblStaff.FindAsync(id);
+            if (employee == null)
+                return BadRequest("employee not found.");
+
+            context.tblStaff.Remove(employee);
+
+            await context.SaveChangesAsync();
+
+            return Ok(employee);
+        }
+    }
+}
---
+++ EmployeexDataAPI/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace EmployeexDataAPI
+{
+	public class Employee
+	{
+		public Int64 ID { get; set; }
+
+		public string EmployeeNumber { get; set; } = string.Empty;
+
+		public string Name { get; set; } = string.Empty;
+
+		public string Surname { get; set; } = string.Empty;
+
+		public string Category { get; set; } = string.Empty;
+
+		public string Department { get; set; } = string.Empty;
+
+		public string SubUnit { get; set; } = string.Empty;
+
+		public string JobTitle { get; set; } = string.Empty;
+
+		public string WorkPlaceState { get; set; } = string.Empty;
+
+		public string Terminated { get; set; } = string.Empty;
+
+	}
+}
+
